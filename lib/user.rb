bc8923792c3e8a64724f6451489da8e1fbd7ce2c
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-19 14:18:34 +0100

    Adds unit test for signup

--- lib/user.rb
+++ lib/user.rb
@@ -9,4 +9,16 @@ class User
     @password = password
   end
 
+  def self.create(name:, email:, password:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+
+    result = connection.exec("INSERT INTO users (email, password, real_name) VALUES ('#{email}', '#{password}', '#{name}') RETURNING user_id, email, password, real_name;")
+    User.new(id: result[0]['user_id'], name: result[0]['real_name'], email: result[0]['email'], password: result[0]['password'])
+
+  end
+
 end
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -1,6 +1,6 @@
 require 'user'
 
-describe User do 
+describe User do
 
   it 'returns its id, name, email and password' do
     user = User.new(id: 1, name: 'Test', email: 'test@example.com', password: 'password123')
@@ -10,7 +10,12 @@ describe User do
     expect(user.password).to eq("password123")
   end
 
-  it 'signs up a user' do 
-    User.sign_up(name: 'Test', email: 'test@example.com', password: 'password123')
-    
+  it 'signs up a user' do
+    user = User.create(name: 'Test', email: 'test@example.com', password: 'password123')
+
+    expect(user).to be_a User
+    expect(user.name).to eq 'Test'
+    expect(user.email).to eq 'test@example.com'
+    # expect(user.password).to eq 'password123'
+  end
 end
