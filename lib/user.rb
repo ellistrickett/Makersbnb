a9168e8df78bd34ac7fadb624a25634a15a40cb7
Ellis Trickett <ellis.trickett@gmail.com>
2020-05-19 11:48:27 +0100

    test pass for user class instances

---
+++ lib/user.rb
@@ -0,0 +1,12 @@
+class User
+
+  attr_reader :id, :name, :email, :password
+
+  def initialize(id:, name:, email:, password:)
+    @id = id
+    @name = name
+    @email = email
+    @password = password
+  end
+
+end
---
+++ spec/user_spec.rb
@@ -0,0 +1,12 @@
+require 'user'
+
+describe User do 
+
+  it 'returns its id, name, email and password' do
+    user = User.new(id: 1, name: 'Test', email: 'test@example.com', password: 'password123')
+    expect(user.id).to eq(1)
+    expect(user.name).to eq("Test")
+    expect(user.email).to eq('test@example.com')
+    expect(user.password).to eq("password123")
+  end
+end
