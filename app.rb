1d34eb53a75f400facaec9dec7e08cc638c106c6
Ed <lookatemail@gmail.com>
2020-05-19 12:35:28 +0100

    add feature to list spaces

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/space'
 
 class MakersBnB < Sinatra::Base
   get '/' do
@@ -6,11 +7,17 @@ class MakersBnB < Sinatra::Base
   end
 
   get '/makersbnb' do
+    @space = Space.view_spaces
     erb :'makersbnb/space'
   end
 
   get '/makersbnb/add-space' do
     erb :'makersbnb/add_space'
+  end
+
+  post '/makersbnb' do
+    Space.add_space(space_name: params[:name], description: params[:description], price: params[:price], dates_available: params[:dates_available])
+    redirect '/makersbnb'
   end
 
   run! if app_file == $0
--- spec/features/space_spec.rb
+++ spec/features/space_spec.rb
@@ -1,11 +1,20 @@
 feature 'add space to MakersBnB' do
-  scenario 'able to add space' do
+  scenario 'able to reach form to add space' do
     visit('/makersbnb')
     click_button 'Add Space'
     expect(current_path).to eq '/makersbnb/add-space'
     expect(page).to have_content 'Add New Space'
   end
 
-
+  scenario 'able to add space' do
+    visit('/makersbnb/add-space')
+    fill_in 'name', with: 'Room'
+    fill_in 'description', with: 'Room Description'
+    fill_in 'price', with: 50.00
+    fill_in 'dates_available', with: '01.01'
+    click_button 'Add Space'
+    expect(current_path).to eq '/makersbnb'
+    expect(page).to have_content 'Room Description'
+  end
 
 end
