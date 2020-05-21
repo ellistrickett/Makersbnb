f8c9572fad40a099a5be56c55ed57ee56c8da9dc
Ed <lookatemail@gmail.com>
2020-05-21 14:51:00 +0100

    able to approve and decline requests

--- lib/request.rb
+++ lib/request.rb
@@ -4,21 +4,19 @@ require_relative 'connect_to_database'
 class Request
 
   def self.request_space(user_id:, space_id:, date:)
-    connect_to_database.exec("INSERT INTO requests (user_id, space_id, date) VALUES('#{user_id}', '#{space_id}', '#{date}');")
+    connect_to_database.exec("INSERT INTO requests (user_id, space_id, date, status) VALUES('#{user_id}', '#{space_id}', '#{date}', 'Pending');")
   end
 
   def self.view_request_space
     connect_to_database.exec("SELECT * FROM requests")
   end
 
-  private
+  def self.approve_space(user_id:, space_id:)
+    connect_to_database.exec("UPDATE requests SET status = 'Request Approved' WHERE user_id = '#{user_id}' AND space_id = '#{space_id}';")
+  end
 
-  def self.connect_to_database
-    if ENV['ENVIRONMENT'] == 'test'
-      PG.connect dbname: 'makersbnb_test'
-    else
-      PG.connect dbname: 'makersbnb'
-    end
+  def self.decline_space(user_id:, space_id:)
+    connect_to_database.exec("UPDATE requests SET status = 'Request Declined' WHERE user_id = '#{user_id}' AND space_id = '#{space_id}';")
   end
 
 end
--- spec/unit/request_spec.rb
+++ spec/unit/request_spec.rb
@@ -13,6 +13,7 @@ describe Request do
         expect(request_object['user_id']).to eq '1'
         expect(request_object['space_id']).to eq '12'
         expect(request_object['date']).to eq '010212'
+        expect(request_object['status']).to eq 'Pending'
       end
     end
 
@@ -28,9 +29,45 @@ describe Request do
         expect(request_object['user_id']).to eq '5'
         expect(request_object['space_id']).to eq '18'
         expect(request_object['date']).to eq '010212'
+        expect(request_object['status']).to eq 'Pending'
       end
     end
 
   end
+
+  describe '.approve_request_space' do
+
+    it 'able to approve requested spaces' do
+      Request.request_space(user_id: 5, space_id: 18, date:'010212')
+      Request.approve_space(user_id: 5, space_id: 18)
+      request = Request.view_request_space
+
+      request.each do |approve_object|
+        expect(approve_object['user_id']).to eq '5'
+        expect(approve_object['space_id']).to eq '18'
+        expect(approve_object['date']).to eq '010212'
+        expect(approve_object['status']).to eq 'Request Approved'
+      end
+    end
+
+  end
+
+  describe '.approve_request_space' do
+
+    it 'able to approve requested spaces' do
+      Request.request_space(user_id: 5, space_id: 18, date:'010212')
+      Request.decline_space(user_id: 5, space_id: 18)
+      decline = Request.view_request_space
+
+      decline.each do |decline_object|
+        expect(decline_object['user_id']).to eq '5'
+        expect(decline_object['space_id']).to eq '18'
+        expect(decline_object['date']).to eq '010212'
+        expect(decline_object['status']).to eq 'Request Declined'
+      end
+    end
+
+  end
+
 
 end
