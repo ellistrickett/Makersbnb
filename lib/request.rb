f17b7d5590f3f0304d9b12592ce5af2e6a05f933
Ed <lookatemail@gmail.com>
2020-05-21 07:21:47 +0100

    able to confirm booking

--- lib/request.rb
+++ lib/request.rb
@@ -1,4 +1,17 @@
 class Request
 
+  def self.approve_space(id:)
+    connect_to_database.exec("UPDATE booking SET requests = 'Booking Confirmed' WHERE space_id = #{id};")
+  end
+
+  private
+
+  def self.connect_to_database
+    if ENV['ENVIRONMENT'] == 'test'
+      PG.connect dbname: 'makersbnb_test'
+    else
+      PG.connect dbname: 'makersbnb'
+    end
+  end
 
 end
